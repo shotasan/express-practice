9a63faa29a7fc22f8ea1cb7c409ea32a04c9cbf7
shotasan <wondermoai@gmail.com>
2019-12-18 15:29:03 +0900

    5-1 クエリパラメーターの利用

--- routes/hello.js
+++ routes/hello.js
@@ -2,9 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res, next) => {
+  // クエリパラメーターの受け取り
+  var name = req.query.name;
+  var pass = req.query.pass;
   var data = {
     title: 'Hello',
-    content: 'これは、サンプルのコンテンツです。<br>this is sample content.'
+    content: `あなたの名前は${name}です。<br>パスワードは${pass}です。`
   };
   res.render('hello', data);
 });
