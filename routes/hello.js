213ffb5a9605961e96246ee9a64e5f073ac679a2
shotasan <wondermoai@gmail.com>
2019-12-18 16:06:13 +0900

    5-4 フォームの利用

--- routes/hello.js
+++ routes/hello.js
@@ -12,4 +12,13 @@ router.get('/', (req, res, next) => {
   res.render('hello', data);
 });
 
+router.post('/post', (req, res, next) => {
+  var msg = req.body['message'];
+  var data = {
+    title: 'Hello!',
+    content: `あなたは${msg}と送信しました。`
+  }
+  res.render('hello', data);
+})
+
 module.exports = router;
