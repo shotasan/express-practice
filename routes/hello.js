3a539c26d79950ba1e8aacaa0b32099fa2d048bd
shotasan <wondermoai@gmail.com>
2019-12-19 15:56:49 +0900

    5-11 apiの利用

--- routes/hello.js
+++ routes/hello.js
@@ -1,28 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const NewsApi = require('newsapi');
+const newsapi = new NewsApi('b76e25b055d846c2b2ca80dca43a2e8f');
 
 router.get('/', (req, res, next) => {
-  var msg = '何か書いてください';
-  // セッション情報の取得
-  if (req.session.message != undefined) {
-    msg = `Last Message: ${req.session.message}`;
-  }
-  var data = {
-    title: 'Hello',
-    content: msg
-  };
-  res.render('hello', data);
-});
-
-router.post('/post', (req, res, next) => {
-  var msg = req.body['message'];
-  // セッション情報の登録
-  req.session.message = msg;
-  var data = {
-    title: 'Hello!',
-    content: `Last Message: ${req.session.message}`
-  };
-  res.render('hello', data);
+  newsapi.v2.topHeadlines({
+    q: 'trump',
+    category: 'politics',
+    language: 'en',
+    country: 'us'
+  }).then(response => {
+    var data = {
+      title: 'Hello!',
+      content: response.articles
+    }
+    res.render('hello', data);
+  })
 });
 
 module.exports = router;
