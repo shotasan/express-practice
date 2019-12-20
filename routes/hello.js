d383d04c337422f5b1047ed595efd35dc226aeaf
shotasan <wondermoai@gmail.com>
2019-12-20 18:35:15 +0900

    5-14 mysqlの利用

--- routes/hello.js
+++ routes/hello.js
@@ -1,21 +1,36 @@
 var express = require('express');
 var router = express.Router();
-const NewsApi = require('newsapi');
-const newsapi = new NewsApi('b76e25b055d846c2b2ca80dca43a2e8f');
+var mysql = require('mysql');
+
+// MySQLの設定情報
+var mysql_setting = {
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'my-nodeapp-db'
+}
 
 router.get('/', (req, res, next) => {
-  newsapi.v2.topHeadlines({
-    q: 'trump',
-    category: 'politics',
-    language: 'en',
-    country: 'us'
-  }).then(response => {
-    var data = {
-      title: 'Hello!',
-      content: response.articles
-    }
-    res.render('hello', data);
-  })
-});
+  // コネクションの用意
+  var connection = mysql.createConnection(mysql_setting);
+
+  // データベースに接続
+  connection.connect();
+
+  // データの取り出し
+  connection.query("SELECT * from mydata",
+    function (error, results, fields) {
+      // データベースアクセス完了時の処理
+      if (error == null) {
+        var data = { title: 'mysql', content: results }
+        res.render('hello', data);
+      } else {
+        throw error;
+      }
+    });
+
+  // 接続を解除
+  connection.end();
+})
 
 module.exports = router;
