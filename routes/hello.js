a2d7a25baa9a04c94819d97fbcd8c87f52468de3
shotasan <wondermoai@gmail.com>
2019-12-20 19:02:58 +0900

    5-16 レコードの作成

--- routes/hello.js
+++ routes/hello.js
@@ -23,13 +23,41 @@ router.get('/', (req, res, next) => {
       // データベースアクセス完了時の処理
       if (error == null) {
         var data = { title: 'mysql', content: results }
-        res.render('hello', data);
+        res.render('hello/index', data);
       } else {
         throw error;
       }
     });
 
   // 接続を解除
+  connection.end();
+});
+
+router.get('/add', (req, res, next) => {
+  var data = {
+    title: 'Hello/Add',
+    content: '新しいレコードを入力'
+  }
+  res.render('hello/add', data);
+});
+
+router.post('/add', (req, res, next) => {
+  var nm = req.body.name;
+  var ml = req.body.mail;
+  var ag = req.body.age;
+  var data = { 'name': nm, 'mail': ml, 'age': ag };
+
+  var connection = mysql.createConnection(mysql_setting);
+
+  connection.connect();
+
+  connection.query('insert into mydata set ?', data,
+    function (error, results, fields) {
+      if (error) throw error;
+      res.redirect('/hello');
+    }
+  );
+
   connection.end();
 })
 
