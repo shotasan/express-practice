c10bf2aac73b9abff61777e635c68f08887ea4fe
shotasan <wondermoai@gmail.com>
2019-12-23 20:22:37 +0900

    6-4 バリデーションの利用

--- routes/hello.js
+++ routes/hello.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
+var { check, validationResult } = require('express-validator');
 
 // MySQLの設定情報
 var mysql_setting = {
@@ -36,30 +37,51 @@ router.get('/', (req, res, next) => {
 router.get('/add', (req, res, next) => {
   var data = {
     title: 'Hello/Add',
-    content: '新しいレコードを入力'
+    content: '新しいレコードを入力',
+    form: { name: '', mail: '', age: 0 }
   }
   res.render('hello/add', data);
 });
 
-router.post('/add', (req, res, next) => {
-  var nm = req.body.name;
-  var ml = req.body.mail;
-  var ag = req.body.age;
-  var data = { 'name': nm, 'mail': ml, 'age': ag };
-
-  var connection = mysql.createConnection(mysql_setting);
-
-  connection.connect();
-
-  connection.query('insert into mydata set ?', data,
-    function (error, results, fields) {
-      if (error) throw error;
-      res.redirect('/hello');
+router.post('/add', [
+  check('name', 'NAMEは必ず入力してください。').notEmpty(),
+  check('mail', 'MAILはメールアドレスを記入してください。').isEmail(),
+  check('age', 'AGEは年齢（整数）を入力ください。').isInt()
+], (req, res, next) => {
+  const result = validationResult(req);
+  if (!result.isEmpty()) {
+    var re = '<ul class="error">';
+    var result_arr = result.array();
+    for (var n in result_arr) {
+      re += `<li>${result_arr[n].msg}</li>`
     }
-  );
+    re += '</ul>';
+    var data = {
+      title: 'Hello/add',
+      content: re,
+      form: req.body
+    }
+    res.render('hello/add', data);
+  } else {
+    var nm = req.body.name;
+    var ml = req.body.mail;
+    var ag = req.body.age;
+    var data = { 'name': nm, 'mail': ml, 'age': ag };
 
-  connection.end();
-})
+    var connection = mysql.createConnection(mysql_setting);
+
+    connection.connect();
+
+    connection.query('insert into mydata set ?', data,
+      function (error, results, fields) {
+        if (error) throw error;
+        res.redirect('/hello');
+      }
+    );
+
+    connection.end();
+  }
+});
 
 router.get('/show', (req, res, next) => {
   var id = req.query.id;
